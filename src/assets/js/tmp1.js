2548acf6cf5bd78c0561e308f96d0c4fd7184129
Gerson Santos <jhxn07@gmail.com>
2022-07-02 18:52:15 -0500

    incialiizaciion del proyecto en repo

---
+++ src/assets/js/tmp1.js
@@ -0,0 +1,242 @@
+export { listC };
+
+
+// se crea un json 
+const listC = [
+   
+	{
+		titulo: '1x01: BLANCO YANKEE',
+		link1: 'https://www48.zippyshare.com/v/bifIIQEw/file.html',
+		link2: 'https://www93.zippyshare.com/v/0KbOY2D0/file.html',
+		link3: 'https://www104.zippyshare.com/v/uQZ1xHAd/file.html',
+		img: '../assets/image/1572109073.png',
+	},
+	{
+		titulo: '1x02: ATERRIZAJE FORZOSO',
+		link1:
+			'https://mega.nz/#!2ssgzSzZ!C0FiKiZb7m7nFh_ukBUpAb324D8u_2lWxuYkQoMV914',
+		link2:
+			'https://mega.nz/#!boFBhIAA!dPAfLAUGIKm_MCKu4U4_RamPpLjPoo1IeXIeblKTv9c',
+		link3:
+			'https://mega.nz/#!e19wRYhS!53OBRia2DwLQen3cC0iwER82S7v0osN7or3Pz7ulbvE',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x03: LOBO DE MAR',
+		link1:
+			'https://mega.nz/#!Ottnwa6Y!4kRk3KFVoOTu1tGArL5v2kNQalcaUy_ZOlPLHB0M1-A',
+		link2:
+			'https://mega.nz/#!nkFDzRaT!Oa-At63IA4up3JNiywJi0VmbtT4KMxygCKvXtROeWv8',
+		link3:
+			'https://mega.nz/#!2511AJLI!zhWK682NSS0XwF814bvm2LRllKnE8iRcZn9PNALtXnw',
+		img: './images/1572109073.png',
+	},
+
+	{
+		titulo: '1x04: LOS INMORTALES',
+		link1:
+			'https://mega.nz/#!K8kDCYiK!0CsURjHTRxyTTFWw2cnmpkwy49pshf4cAUDkW-6ItME',
+		link2:
+			'https://mega.nz/#!X11FUAgA!VtB-OwMo_G855_rWI1i14HDYkL9XDoqzEe0jSxVn3Ls',
+		link3:
+			'https://mega.nz/#!v0VyTZgT!L3EJJ6iLuZkUVPc6COXWOEVUojuyduXFLGSm9NqiZuw',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x05: LA MALDICIÓN',
+		link1:
+			'https://mega.nz/#!3x0HzQ6Q!T1O0QvurEDAJtiGNIqM1XTIBGGRnXFyNvoFZWPuv69c',
+		link2:
+			'https://mega.nz/#!20VQWa4J!akBX-DqeSvmY-DBdF2jn32odafwga3xn1SKYQrrn4ys',
+		link3:
+			'https://mega.nz/#!Tt1XyJya!Rdx8i7i9_qtvICMV4QPTKOj6kXsmX4xvZsguhpCaILA',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x06: LOCURA EN ALTA MAR',
+		link1:
+			'https://mega.nz/#!ywUy2YBa!C540jzKxuHAEzrtOvD9av2fukJ2cTub_pmXx4yfclkE',
+		link2:
+			'https://mega.nz/#!bkEgDBII!Psi7-ZhcyOtXCpAWcAS5bzqM_CvqleBzR6FHTjg0AXs',
+		link3:
+			'https://mega.nz/#!ntMXGJCS!Y5cyb_kAlxDadQlcjwmUjNrlpeyxBr-iHMbpBWf9-bU',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x07: SUBMARINO EN ALERTA',
+		link1:
+			'https://mega.nz/#!PhEHwYCA!lQleadRgYha2Sxv4hng1gPz8_ZvY81SK6C9ANVqNsYU',
+		link2:
+			'https://mega.nz/#!K5cw2bAT!SPvYXl1KL5RDzNeRBHXtVg64kP8UEsXz5K_JhFaTAPw',
+		link3:
+			'https://mega.nz/#!GwcymATL!fyDUGLopLqjzT3FRWXYZHiS8MN9uU2ceSZDRyQUaruA',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x08: SEGURIDAD MINIMA',
+		link1:
+			'https://mega.nz/#!qh1mjDoB!L3LiGquTs1k6OjKO1niM2pqQWThQng-egBnErLk1IsQ',
+		link2:
+			'https://mega.nz/#!CpUC2R4R!_AhDCrji0dJsUgCYxjepkA9jDIh6M8GFhbw26JNy-2g',
+		link3:
+			'https://mega.nz/#!H5cFxQpS!d17-sxyjqL8QmmQ5ewFiGacobNrzsYzEA_fHB-pZxUA',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x09: MARINO MUERTO',
+		link1:
+			'https://mega.nz/#!fxMSDRiL!2InzlTM8jF2yHEaMAcNmOu6-CG5PDp6QtdQpM4ZHyMw',
+		link2:
+			'https://mega.nz/#!HpsUWZrT!BnbeYsR_ju3MhG7ZYgNqnQMObvSzzAk4Iovo-u8sBNA',
+		link3:
+			'https://mega.nz/#!SlMX3IZL!x7htxJkDHAmTmKtC4wLNH6xabEq2Izv3aPkVTShob-4',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x10: DADA POR MUERTA',
+		link1:
+			'https://mega.nz/#!z8MhCKwa!znVq4JGNP3oLKWdDP7974xRJZwB-ZSxTO3lFgQjD4j0',
+		link2:
+			'https://mega.nz/#!b98WSLRa!emMYxrLmgOEWUpN-eDllnPJyqRxQPS6PIr2h1kQcFDU',
+		link3:
+			'https://mega.nz/#!D08yzaRQ!KVcaozxuvsV7vseInmuBI6SqqIK1pUbmBGncaYRIpdM',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x11: OJO ESPIÁ',
+		link1:
+			'https://mega.nz/file/71MlBIJK#3Ez-Vp8mc-HZclhm9zo7q8e4ivoAKFCcwbbiZF5YMS0',
+		link2:
+			'https://mega.nz/#!Ls1U0ABQ!OPqZxVABceD7zvAW3pJI2oO089WpKAeScvNMhvgamx8',
+		link3:
+			'https://mega.nz/#!L1dnQBBI!kPGlKveC8oXcmnDxNwNBqOwFh5-H3FDE_xkEAag8yQU',
+		img: './images/1572109073.png',
+	},
+
+	{
+		titulo: '1x12: MI OTRO PIE IZQUIERDO',
+		link1:
+			'https://mega.nz/#!m5kGkJYS!AeDmQbJp-J9Sf_7N8k6xPNdCv3WQRKzcUw8kHNpYUOY',
+		link2:
+			'https://mega.nz/#!j8sCSJRR!jeFT8d_pEf2wlgQCH_ihZQBbqPJxkpE3fuP6RXoCRzo',
+		link3:
+			'https://mega.nz/#!q1sDjY5Q!yTOat9jBYcVm5tRnOdjZYpcIzTUChWbZ7wq72LLrJD8',
+		img: './images/1572109073.png',
+	},
+
+	{
+		// (SOLO DEBEN DESCARGAR 2 ARCHIVOS WINRAR PARA ESTE EPISODIO)
+		titulo: '1x13: UN DISPARO, UN HOMICIDIO',
+		link1:
+			'https://mega.nz/#!OksQWAgQ!GtXYmM7ZzcMMl0cWWGDiJ6GY3qAUom2nKN4Qnr-v8mo',
+		link2:
+			'https://mega.nz/#!nxMWCCpB!sf6oFN8VPSVnBwqMw2f_Knh72IBzdKhoWp5Nb-igJuI',
+		img: './images/1572109073.png',
+	},
+
+	{
+		titulo: '1x14: EL BUEN SAMARITANO',
+		link1:
+			'https://mega.nz/#!DgEHlQyA!kTGorqp-o6GVAdSbh7ikZT5peJnUnz4MFHwgTlyiKq8',
+		link2:
+			'https://mega.nz/#!n8cERLhY!Bn_Eyja0FvWVZXEfvd9PlDDqcsw-zl_t_Rr1dykybH8',
+		link3:
+			'https://mega.nz/#!StMwyB6Q!h7kPZB6hKZOl63sMtH8zu1PX_SmVKZKqLaMUC4hMrVg',
+		img: './images/1572109073.png',
+	},
+
+	{
+		titulo: '1x15: ENIGMA',
+		link1:
+			'https://mega.nz/#!yp9h0J6B!w9xhtg61AKEZexddl583b_E_z3nL1V1nJelllc1GWxI',
+		link2:
+			'https://mega.nz/#!3o8wmQLa!Pk7qRD6SKg0_gk1P3z3t69GmtNxf54ttStBIH1L4bKY',
+		link3:
+			'https://mega.nz/#!ehMVGSwa!c8ZrgyVuVlkTWJ5rf_j1lktJfE5qOh5CqWOpiqWZtVQ',
+		img: './images/1572109073.png',
+	},
+
+	{
+		titulo: '1x16: PESADILLA',
+		link1:
+			'https://mega.nz/#!q0tTwa7R!TV97hgDTqBpDkoaf4GrckKKuyyPKhdFvWZhDpI_CFnE',
+		link2:
+			'https://mega.nz/#!vxMBUKAb!YOAKEozSiQqHfc6lB3IW43ZpyQ-a5ZG7__3flLSOLrk',
+		link3:
+			'https://mega.nz/#!68U2VAJT!9odmLJPDplj1P0EbyNsWFzS3KVLl_9514ds4M9YaJqU',
+		img: './images/1572109073.png',
+	},
+
+	{
+		titulo: '1x17: LA VERDAD ESTÁ ALLÁ AUERA',
+		link1:
+			'https://mega.nz/#!TkEjUQSZ!b7rGVYS0PgxKL45hgL9vEMlpON0OtECAL7xnrfzNXfs',
+		link2:
+			'https://mega.nz/#!K1NXCDyJ!axofpN9ciUveKY4AtCC8wRdUkzEa2cNgJp6ouOWpbt8',
+		link3:
+			'https://mega.nz/#!aodhlKoS!X79-Hwmyb7VTzhB75o0X9IelAp1-ptqlhiJTb3Rl9_Y',
+		img: './images/1572109073.png',
+	},
+
+	{
+		titulo: '1x18: EX SEAL',
+		link1:
+			'https://mega.nz/#!z4UGRJKT!btvkOIgD6RtbjluOAng9HvvrrSMKIZ24bo3BbNANicY',
+		link2:
+			'https://mega.nz/#!rocHXRRA!NKCuF6ScXoaWQxQgHPWFpApv_-hJwPSSEtf6W6_EqpA',
+		link3:
+			'https://mega.nz/#!ulc0yYzb!2LyZ8tQCbfFcYj7IUmjHlW-dp9HRH8Go-6FrNJqRA5o',
+		img: './images/1572109073.png',
+	},
+
+	{
+		titulo: '1x19: UN CADAVER QUE HABLA',
+		link1:
+			'https://mega.nz/#!C1lXiCzJ!gZxnFOMB15bgirjAoSf-PhXLBWRXs7bs9jKNuRX1Ieo',
+		link2:
+			'https://mega.nz/#!69MSCDAC!1Et0_Ur0pz-7zkJjZomXsVOQmNnjccVZRpBNe6S2vos',
+		link3:
+			'https://mega.nz/#!zt1UULZB!kSn2DijFb-ZjCO3Ylqvx9kWwZx0EifR0548slNn5cL8',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x20: EXTRAVIADO',
+		link1:
+			'https://mega.nz/#!SxVkTY7T!-dX0YtinxQ0sBE3jFlWWCzIdqV-p4lOa9wLIARuqWjk',
+		link2:
+			'https://mega.nz/#!H0t10BSb!bOrW-01pF6bGvGS_ur1fJcXZ7HTVlT8CHbqTQpUHdwk',
+		link3:
+			'https://mega.nz/#!PgN2lAiS!ovE8zbepONRwaZ7eJGLBIRnivceYS3C_7XdCh4ia-kI',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x21: DESICIÓN DIFÍCIL',
+		link1:
+			'https://mega.nz/#!Ot1X0DqD!JoKctz8cqbRvjPKF7g-oQ_1BZlcjO59x91SgwpBjQAY',
+		link2:
+			'https://mega.nz/#!yksExLYK!-lni5mZq8ueOQR4PJY3-RI1HRskA__n2vFSlQOwObYY',
+		link3:
+			'https://mega.nz/#!Xg8CUaBR!pyDrjkIq4DT9mbgJS--uhLFS3YJADzmMM2pYQBpv_e4',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x22: EL VÍNCULO DÉBIL',
+		link1:
+			'https://mega.nz/#!q1dySDxL!F8iZfBzoXLYPsLwOUj5UYNzUDIHFt7glH9uGHByv8Zc',
+		link2:
+			'https://mega.nz/#!ys8TQJqS!-ikr_Qm61YnzoKIXRVVkieNR2W73L2gOpt4qNLgDk5M',
+		link3:
+			'https://mega.nz/#!qg1AjKzD!Rb45mjC1KwhUT6Gettn2J2VL5-o1GcpZEgnD2zP-YUU',
+		img: './images/1572109073.png',
+	},
+	{
+		titulo: '1x23: TOQUE DE ALBORADA - FINAL DE TEMPORADA',
+		link1:
+			'https://mega.nz/#!fkV3RYrD!w_cvrXPIcYFva8dimNgSlWoA0nVsWce64t5J_KTPCLY',
+		link2:
+			'https://mega.nz/#!69FkyBhC!LYnmGojdhqD0EIuYQO8dEe-rP0_EIIc8lSWkJUf2oWI',
+		link3:
+			'https://mega.nz/#!HoEFEL5D!V3TX0os9SXk9ImRj-JXfwMJ6oeGQv0WaSygYLlnu5hI',
+		img: './images/1572109073.png',
+	},
+];
---
+++ src/assets/js/tmp12.js
@@ -0,0 +1,177 @@
+export {temp12}
+
+const temp12 =[
+
+{
+titulo:'12x01: 20 KILÓMETROS',
+link1:'https://www83.zippyshare.com/v/qTcLg4BT/file.html',
+link2:'https://www42.zippyshare.com/v/vmbToQ3F/file.html',
+link3:'https://www115.zippyshare.com/v/072q5Ndr/file.html',
+img: './images/t12.png'
+}
+,
+{
+titulo:'12x02: MATA AL MENSAJERO',
+link1:'https://www78.zippyshare.com/v/FtoJfNm1/file.html',
+link2:'https://www111.zippyshare.com/v/gqqr7aYZ/file.html',
+link3:'https://www14.zippyshare.com/v/0xe6XUuK/file.html',
+img: './images/t12.png',
+}
+,
+{
+titulo:'12x03: ASÍ CUENTA LA HISTORIA',
+link1:'https://www49.zippyshare.com/v/fNxQMbcm/file.html',
+link2:'https://www7.zippyshare.com/v/y5rONMdm/file.html',
+link3:'https://www14.zippyshare.com/v/XSunrbpz/file.html',
+img: './images/t12.png'
+}
+,
+{
+titulo:'12x04: ESTRANGULAMIENTO',
+link1:'https://www48.zippyshare.com/v/bifIIQEw/file.html',
+link2:'https://www93.zippyshare.com/v/0KbOY2D0/file.html',
+link3:'https://www104.zippyshare.com/v/uQZ1xHAd/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x05: EL SAN DOMINICK',
+link1:'https://www77.zippyshare.com/v/r2HtgQis/file.html',
+link2:'https://www5.zippyshare.com/v/MHJ4b24R/file.html',
+link3:'https://www8.zippyshare.com/v/bwQh8utZ/file.html',
+img: './images/t12.png'
+},
+{
+titulo:'12x06: CONTROL INFANTIL',
+link1:'https://www109.zippyshare.com/v/ScvovR1p/file.html',
+link2:'https://www109.zippyshare.com/v/VpTVyMfd/file.html',
+link3:'https://www119.zippyshare.com/v/1T6Magzp/file.html',
+img: './images/t12.png'
+},
+{
+titulo:'12x07: LOS RASTREADORES',
+link1:'https://www6.zippyshare.com/v/KuQF4rs6/file.html',
+link2:'https://www6.zippyshare.com/v/EbbrIABP/file.html',
+link3:'https://www119.zippyshare.com/v/86Z0bwxx/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x08: SIEMPRE FUERTE',
+link1:'https://www50.zippyshare.com/v/0hVPhJcm/file.html',
+link2:'https://www50.zippyshare.com/v/HwrzxPZk/file.html',
+link3:'https://www10.zippyshare.com/v/gjojL5Mn/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x09: CONFINADOS',
+link1:'https://www106.zippyshare.com/v/uUF5PPLY/file.html',
+link2:'https://www106.zippyshare.com/v/DyKZYuCL/file.html',
+link3:'https://www10.zippyshare.com/v/2gwkwazp/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x10: REGLAS DE LA CASA',
+link1:'https://www7.zippyshare.com/v/Jm4B1Rij/file.html',
+link2:'https://www7.zippyshare.com/v/BKD0XGyG/file.html',
+link3:'https://www4.zippyshare.com/v/lMtiikk3/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x11: JAQUE',
+link1:'https://www120.zippyshare.com/v/Es1iD7SZ/file.html',
+link2:'https://www120.zippyshare.com/v/CnCuFvnn/file.html',
+link3:'https://www4.zippyshare.com/v/Hd9JwXml/file.html',
+img: './images/t12.png',
+},
+
+{
+titulo:'12x12: ENEMIGO EN CASA',
+link1:'https://www26.zippyshare.com/v/sxMrZ7IU/file.html',
+link2:'https://www26.zippyshare.com/v/BpVhDjdi/file.html',
+link3:'https://www12.zippyshare.com/v/PTlt2sN8/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x13: CONSTRUIMOS, LUCHAMOS',
+link1:'https://www18.zippyshare.com/v/ypUYj354/file.html',
+link2:'https://www18.zippyshare.com/v/ERjn21cu/file.html',
+link3:'https://www12.zippyshare.com/v/ABEmGaHS/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x14: CADENCIA',
+link1:'https://www15.zippyshare.com/v/Dj9jttbl/file.html',
+link2:'https://www15.zippyshare.com/v/QrtZEodw/file.html',
+link3:'https://www14.zippyshare.com/v/2I8UHCof/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x15: CLAUSTROFOBIA',
+link1:'https://www117.zippyshare.com/v/vowthKlJ/file.html',
+link2:'https://www117.zippyshare.com/v/5ah4pfwJ/file.html',
+link3:'https://www14.zippyshare.com/v/QEprJN66/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x16: DESTELLO DEL PASADO',
+link1:'https://www80.zippyshare.com/v/mw9NgQOD/file.html',
+link2:'https://www70.zippyshare.com/v/y2Vr38kk/file.html',
+link3:'https://www51.zippyshare.com/v/ju19Ap7I/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x17: EL HÁBIL ESTAFADOR',
+link1:'https://www66.zippyshare.com/v/Wx6Zb8Gs/file.html',
+link2:'https://www51.zippyshare.com/v/85fastkT/file.html',
+link3:'https://www53.zippyshare.com/v/O24Uzz4P/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x18: ACTUALIZACIÓN DE ESTADO',
+link1:'https://www91.zippyshare.com/v/s4UExNh2/file.html',
+link2:'https://www91.zippyshare.com/v/1o8xz7oQ/file.html',
+link3:'https://www95.zippyshare.com/v/TlWzQhu4/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x19: PACIENCIA',
+link1:'https://www101.zippyshare.com/v/vmfeJLCq/file.html',
+link2:'https://www101.zippyshare.com/v/QnnVKX2a/file.html',
+link3:'https://www95.zippyshare.com/v/dQATGamk/file.html',
+img: './images/t12.png',
+},
+{ 
+titulo:'12x20: EL BUEN SAMARITANO',
+link1:'https://www71.zippyshare.com/v/MjWN5WMQ/file.html',
+link2:'https://www71.zippyshare.com/v/Rr2RxWZq/file.html',
+link3:'https://www29.zippyshare.com/v/tJiwKzF0/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x21: PERDIDOS EN LA TRADUCCIÓN',
+link1:'https://www36.zippyshare.com/v/yVXLhzz4/file.html',
+link2:'https://www36.zippyshare.com/v/A7vO2gSu/file.html',
+link3:'https://www29.zippyshare.com/v/J5wh5vbJ/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x22: TROLL',
+link1:'https://www88.zippyshare.com/v/Qj6qMBTU/file.html',
+link2:'https://www88.zippyshare.com/v/3JXAAI1G/file.html',
+link3:'https://www45.zippyshare.com/v/fFQibVus/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x23: LOS CHICOS PERDIDOS',
+link1:'https://www36.zippyshare.com/v/AUTZie1N/file.html',
+link2:'https://www36.zippyshare.com/v/wn8vlv7Q/file.html',
+link3:'https://www45.zippyshare.com/v/0AiGeMKT/file.html',
+img: './images/t12.png',
+},
+{
+titulo:'12x24: LA TIERRA DEL NUNCA JAMÁS - FINAL DE TEMPORADA',
+link1:'https://www53.zippyshare.com/v/UnNXnZSW/file.html',
+link2:'https://www53.zippyshare.com/v/VcN6akRJ/file.html',
+link3:'https://www93.zippyshare.com/v/md9QPxHp/file.html',
+img: './images/t12.png',
+}
+]
